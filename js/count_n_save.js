5a16a7fbb1b216cb40f45897b5b1403e41a18659
rockydey <coderrocky25@gmail.com>
2022-02-17 11:31:14 +0600

    add error handler

--- js/count_n_save.js
+++ js/count_n_save.js
@@ -24,11 +24,50 @@ document.getElementById('calculate-cost').addEventListener('click', function () 
     // Adding total cost
     const totalCostSum = totalCost(foodCost, rentCost, clothesCost);
 
+    // Error Handling
+    try {
+        if (totalIncome < 0 || foodCost < 0 || rentCost < 0 || clothesCost < 0) {
+            throw 'Please provide a positive integer value.';
+        } else if (isNaN(totalIncome) || isNaN(foodCost) || isNaN(rentCost) || isNaN(clothesCost)) {
+            throw "Please enter an integer value."
+        } else if (totalIncome < totalCostSum) {
+            throw "Costs more than your income."
+        }
+    }
+    catch (error) {
+        if (error == "Costs more than your income.") {
+            document.getElementById('liveAlertExpenses').innerText = error;
+        }
+        else {
+            alert(error);
+        }
+    }
+
+
     // Display value
-    document.getElementById('total-expenses').innerText = totalCostSum;
-    document.getElementById('balance').innerText = remainAmount(totalIncome, totalCostSum);
-
-
+    if (totalIncome >= 0 && foodCost >= 0 && rentCost >= 0 && clothesCost >= 0) {
+        if (totalIncome >= totalCostSum) {
+            document.getElementById('liveAlertExpenses').innerText = '';
+            document.getElementById('total-expenses').innerText = totalCostSum;
+            document.getElementById('balance').innerText = remainAmount(totalIncome, totalCostSum);
+        }
+        else {
+            document.getElementById('total-expenses').innerText = '0';
+            document.getElementById('balance').innerText = totalIncome;
+        }
+    }
+    else if (isNaN(totalIncome) || isNaN(foodCost) || isNaN(rentCost) || isNaN(clothesCost)) {
+        document.getElementById('total-expenses').innerText = '0';
+        if (isNaN(totalIncome)) {
+            document.getElementById('balance').innerText = '0';
+        } else {
+            document.getElementById('balance').innerText = totalIncome;
+        }
+    }
+    else {
+        document.getElementById('total-expenses').innerText = '0';
+        document.getElementById('balance').innerText = totalIncome;
+    }
 });
 
 document.getElementById("save-btn").addEventListener('click', function () {
@@ -46,7 +85,23 @@ document.getElementById("save-btn").addEventListener('click', function () {
     // find remaining balance
     const remainBalanceAmount = remainAmount(totalIncome, totalCostSum);
 
+    // Error Handling
+    try {
+        if (saveAmount > remainBalanceAmount) {
+            throw "Sorry, you don't have enough money.";
+        }
+    }
+    catch (error) {
+        document.getElementById('liveAlertSave').innerText = error;
+    }
+
     // Display outputs
-    document.getElementById('saving-amount').innerText = saveAmount;
-    document.getElementById('remain-balance').innerText = remainAmount(remainBalanceAmount, saveAmount);
+    if (saveAmount <= remainBalanceAmount) {
+        document.getElementById('liveAlertSave').innerText = '';
+        document.getElementById('saving-amount').innerText = saveAmount;
+        document.getElementById('remain-balance').innerText = remainAmount(remainBalanceAmount, saveAmount);
+    } else {
+        document.getElementById('saving-amount').innerText = '0';
+        document.getElementById('remain-balance').innerText = remainBalanceAmount;
+    }
 });
